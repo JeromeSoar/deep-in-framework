253e2d83dc6a8f3b1efa8ee1a482a3846fbfb593
JeromeZhu <jeromezhu666@gmail.com>
2020-01-07 17:38:00 +0800

    初始提交 mybatis 模块

---
+++ mybatis/src/main/java/xin/jerome/study/mybatis/model/Bank.java
@@ -0,0 +1,17 @@
+package xin.jerome.study.mybatis.model;
+
+import lombok.Data;
+
+/**
+ * 银行实体
+ *
+ * @author zhu shaojie
+ * @since 2020年01月07日 17:27
+ */
+@Data
+public class Bank {
+
+    private Integer id;
+    private String name;
+    private String location;
+}
